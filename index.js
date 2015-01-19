28fa6f21bf758bfa1063d92a8acf823a20518dce
faiq <faiqrazarizvi@gmail.com>
2015-01-19 17:10:34 -0500

    fixed scopes so we can access all files

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-var express = require("express")
+var express = require("express") 
   , http = require("http")
-  , fs = require("fs") 
+  , fs = require("fs")
   , passport = require("passport")
-  , goAuth = require("passport-google-oauth").OAuth2Strategy
+  , goAuth = require("passport-google-oauth").OAuth2Strategy 
   , cookieParser = require('cookie-parser')
   , session = require('express-session')
   , trace = require('long-stack-traces')
   , refresh = require('google-refresh-token')
   , User = require('./models/user')
+  , Google = require('./fetchers/Google')
   , router = express()
 
 router.use(session({secret: 'cat'}))
@@ -20,33 +21,35 @@ passport.use(new goAuth({
   clientSecret: process.env["CS"],
   callbackURL: "http://127.0.0.1:3000/auth/google/callback",
   scope: ['https://www.googleapis.com/auth/userinfo.profile',
-  'https://www.googleapis.com/auth/userinfo.email',"https://www.googleapis.com/auth/drive.file"]
+  'https://www.googleapis.com/auth/userinfo.email',"https://www.googleapis.com/auth/drive"]
   },
   function(token, refreshToken, params, profile, done) {
-    User.findOne({token: token}, function (err, user) { 
-      if (err) { 
-        return done(err, user)
-      } else if (!user) { 
-        var now = + new Date
-        var newUser = new User(
-          { token: token, 
-            refreshToken: refreshToken, 
-            expirationDate: now + params.expires_in
-          }
-        )
-        newUser.save(function (err, user) { 
-          console.log('dafsadsfadfsadfs')
-          if (err) {
-            console.log('fuck'); 
-            return console.error(err) 
-          }
-          return done(null, newUser) 
-        }) 
-      } else {
-        console.log('the user was already there hoe')
-        return done(null, err)  
-      }
-    }) 
+    //User.findOne({token: token}, function (err, user) { 
+    //  if (err) { 
+    //    console.log('errrrrrrrr')
+    //    return done(err, user)
+    //  } else if (!user) { 
+    //    var now = + new Date
+    //    var newUser = new User(
+    //      { token: token, 
+    //        refreshToken: refreshToken, 
+    //        expirationDate: now + params.expires_in
+    //      }
+    //    )
+    //    newUser.save(function (err, user) { 
+    //      console.log('dafsadsfadfsadfs')
+    //      if (err) {
+    //        console.log('fuck'); 
+    //        return console.error(err) 
+    //      }
+    //      return done(null, newUser) 
+    //    }) 
+    //    console.log('user not found')
+    //  } else {
+    //    console.log('the user was already there hoe')
+    //  }
+    //}) 
+    return done(null, token)
   })
 )
 
@@ -70,9 +73,10 @@ router.get('/fail', function (req, res) {
 }) 
 
 router.get('/pass',  function (req, res) { 
-  console.log(req.isAuthenticated())
-  console.log(req.user)
-  res.send('pass')
+  var client = new Google(req.user) 
+    , s = client.getAll()
+  s.pipe(res)
+  s.on('error', console.error.bind(console))
 })
 
 router.get("/", function (req, res) { 
