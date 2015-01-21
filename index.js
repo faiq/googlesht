b2b10ab86165722d859cc3350863bb172c654484
faiq <faiqrazarizvi@gmail.com>
2015-01-21 14:18:20 -0500

    sessions and mongo work now

--- index.js
+++ index.js
@@ -1,111 +1,101 @@
-var express = require("express") 
+var express = require("express")
   , http = require("http")
   , request = require("request")
   , fs = require("fs")
   , passport = require("passport")
-  , goAuth = require("passport-google-oauth").OAuth2Strategy 
+  , goAuth = require("passport-google-oauth").OAuth2Strategy
   , cookieParser = require('cookie-parser')
   , session = require('express-session')
-  , trace = require('long-stack-traces')
   , refresh = require('google-refresh-token')
   , JSONStream = require('JSONStream')
   , mongoose = require('mongoose')
   , Google = require('./fetchers/Google')
   , router = express()
 
-mongoose.connect('mongodb://localhost/googleUsers')
-var db = mongoose.connection
-db.on('error', function (err) { 
-  console.error(err)
-}) 
-db.once('open', function () { 
-  console.log('yo')
-  console.log('mongo in da house!')
+mongoose.connection.on('error', function (err) {
+      console.error(err)
 })
 
-var userSchema = mongoose.Schema({
-  token: String,
-  refreshToken: String, 
-  expirationDate: Number
+mongoose.connection.on('open', function (err) {
+    console.log('yo')
+    console.log('mongo in da house!')
 })
-  , User = mongoose.model('user', userSchema)  
+
+mongoose.connect('mongodb://localhost/googleUsers')
+
+var userSchema = mongoose.Schema({
+    id: String,
+    token: String,
+    refreshToken: String,
+    expirationDate: Number
+})
+  , User = mongoose.model('user', userSchema)
+
 
 router.use(session({secret: 'cat'}))
 router.use(passport.initialize())
 router.use(passport.session())
 router.use(cookieParser())
-
+ 
 passport.serializeUser(function(user, done) {
-  done(null, user)
+  done(null, user.id)
 })
 
-passport.deserializeUser(function(user, done) {
-  done(null, user)
+passport.deserializeUser(function(id, done) {
+  User.findOne({id: id}, function (err, user) { 
+    done(err, user)
+  })
 })
-
 passport.use(new goAuth({
   clientID: process.env["KEY"],
   clientSecret: process.env["CS"],
-  callbackURL: "http://127.0.0.1:3000/auth/google/callback",
-  scope: ['https://www.googleapis.com/auth/userinfo.profile',
-  'https://www.googleapis.com/auth/userinfo.email',"https://www.googleapis.com/auth/drive"]
+  callbackURL: "http://127.0.0.1:3000/auth/google/callback"
   },
-  function(token, refreshToken, params, profile, done) {
-    console.log('this is token ' + token)
-    console.log('refresh ' + refreshToken)
-    console.log('params ' + params)
-    console.log('profile ' + profile)
-    User.findOne({token: token}, function (err, user) { 
-      console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-      if (err) { 
-        console.log('errrrrrrrr')
-        return done(err, null)
-      } else if (!user) { 
-        var now = + new Date
-        var newUser = new User(
-          { token: token, 
-            refreshToken: refreshToken, 
-            expirationDate: now + params.expires_in
-          }
-        )
-        newUser.save(function (err, user) { 
-          console.log('dafsadsfadfsadfs')
-          if (err) {
-            console.log('fuck'); 
-            return done(err, null)
-          }
-          return done(null, newUser) 
-          console.log('user not found')
-        }) 
-      } else {
-        console.log('the user was already there hoe')
-        return done(null, user)
-      }
-  }) 
-}))
+  function(aToken, refreshToken, params, profile, done) {
+    process.nextTick(function () { 
+      User.findOne({id: profile.id}, function (err, user) {
+        if (err) return done(err)
+        else if (user) return done(null, user)
+        else { 
+          var newUser = new User()
+          newUser.id = profile.id
+          newUser.token = aToken
+          newUser.refreshToken = refreshToken
+          newUser.expirationDate = Math.floor((+ new Date)/1000 + params.expires_in)
+          newUser.save(function(err) {
+            if (err) throw err;
+            return done(null, newUser);
+          }) 
+        }
+      })
+    })
+  }
+  )
+)
 
 
 router.get("/auth/google", passport.authenticate('google', 
-  { session: false,
+  { 
+    scope: ['https://www.googleapis.com/auth/userinfo.profile',
+    'https://www.googleapis.com/auth/userinfo.email',"https://www.googleapis.com/auth/drive"],
     accessType: 'offline',
-    approvalPrompt: 'force'})) 
+    approvalPrompt: 'force'
+  })) 
 
-router.get("/auth/google/callback",passport.authenticate('google', { failureRedirect: '/fail',  successRedirect : '/pass' }))
+router.get("/auth/google/callback",passport.authenticate('google', {failureRedirect: '/fail',  successRedirect : '/pass' }))
      
 router.get('/fail', function (req, res) { 
   res.send('fail')
 }) 
 
 router.get('/pass',  function (req, res) { 
-  var client = new Google(req.user) 
+  var client = new Google(req.user.token) 
     , resArr = []
-  console.log(req.user)
   client.getAll(function (e, r, b) { 
     b = JSON.parse(b)
+    console.log(b)
     b.items.forEach(function (i) { 
-      if (i["mimeType"] === 'application/vnd.google-apps.spreadsheet') { 
-          resArr.push(i)
-      } 
+      if (i["mimeType"] === 'application/vnd.google-apps.spreadsheet') resArr.push(i)
     })
     res.send(resArr)
   })
